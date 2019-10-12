0e9befaae555fa9b6119b62e82cc2e6560245dc1
Wanchat <wanchatgame@gmail.com>
2019-10-13 02:41:04 +0700

    1. เพิ่ม button, editText(Plain text)
    2.
    เพิ่ม textView ใส่ scrollbars = vertical
    
    3.
    ประกาศ private var ตัวแปรใน mainactivity
    
    4.
    ประกาศ numTimesClicked ใช้กับ onclicklistener
    
    5.
    ใช้ movementMethod
    
    6.
    ลบตัวแปรออกเหลือ textview ไว้ และแก้โค้ดใน onclick

--- app/src/main/java/com/example/kotlintutorial_5_2/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_5_2/MainActivity.kt
@@ -7,33 +7,26 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var userInput: EditText? = null
-    private var button: Button? = null
     private var textView: TextView? = null
-    private var numTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById<EditText>(R.id.editText)
-        button = findViewById<Button>(R.id.button)
+        val userInput: EditText = findViewById<EditText>(R.id.editText)
+        val button: Button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
         textView?.text = ""
         textView?.movementMethod = ScrollingMovementMethod()
 
-        button?.setOnClickListener(object : View.OnClickListener {
+        button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                numTimesClicked += 1
-                textView?.append("The button got tapped $numTimesClicked time")
-                if (numTimesClicked != 1) {
-                    textView?.append("$\n")
-                }else{
-                    textView?.append("\n")
-                }
+                textView?.append(userInput.text)
+                textView?.append("\n")
             }
         })
     }
