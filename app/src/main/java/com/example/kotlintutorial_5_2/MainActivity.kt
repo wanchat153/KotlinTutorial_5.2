cbb903a4ae3137d2a4d57d7db75e1c6192002a91
Wanchat <wanchatgame@gmail.com>
2019-10-13 02:17:10 +0700

    1. เพิ่ม button, editText(Plain text)
    2.
    เพิ่ม textView ใส่ scrollbars = vertical
    
    3.
    ประกาศ private var ตัวแปรใน mainactivity

--- app/src/main/java/com/example/kotlintutorial_5_2/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_5_2/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.kotlintutorial_5_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var userInput: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userInput = findViewById<EditText>(R.id.editText)
+        button = findViewById<Button>(R.id.button)
+        textView = findViewById<TextView>(R.id.textView)
     }
 }
