421363575dcbc5098063cb1a5abb836fa9e569d0
Wanchat <wanchatgame@gmail.com>
2019-10-13 03:00:21 +0700

    1. เพิ่ม button, editText(Plain text)
    2.
    เพิ่ม textView ใส่ scrollbars = vertical
    
    3.
    ประกาศ private var ตัวแปรใน mainactivity
    
    4.
    ประกาศ numTimesClicked ใช้กับ onclicklistener
    
    5.
    ใช้ movementMethod
    
    6.
    ลบตัวแปรออกเหลือ textview ไว้ และแก้โค้ดใน onclick
    
    7.
    เพิ่ม userInput.setText
    
    8.
    เพิ่ม Log และ ประกาศใช้
    
    9.
    สร้าง override fun onStart, onRestoreInstanceState, onResume, onPause, onSaveInstanceState, onStop, onRestart, onDestroy
    และใส่Logทุกอัน

--- app/src/main/java/com/example/kotlintutorial_5_2/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_5_2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlintutorial_5_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.View
@@ -36,5 +37,45 @@ class MainActivity : AppCompatActivity() {
                 userInput.setText("")
             }
         })
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart: called")
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "onRestoreInstanceState: called")
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume: called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause: called")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        Log.d(TAG, "onSaveInstanceState: called")
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop: called")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "onRestart: called")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy: called")
+        super.onDestroy()
     }
 }
