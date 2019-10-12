fb53030f44fb55d05c7b8e282cc478414f53eda4
Wanchat <wanchatgame@gmail.com>
2019-10-13 02:32:09 +0700

    1. เพิ่ม button, editText(Plain text)
    2.
    เพิ่ม textView ใส่ scrollbars = vertical
    
    3.
    ประกาศ private var ตัวแปรใน mainactivity
    
    4.
    ประกาศ numTimesClicked ใช้กับ onclicklistener
    
    5.
    ใช้ movementMethod

--- app/src/main/java/com/example/kotlintutorial_5_2/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_5_2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlintutorial_5_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -21,11 +22,18 @@ class MainActivity : AppCompatActivity() {
         userInput = findViewById<EditText>(R.id.editText)
         button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
 
         button?.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
+            override fun onClick(p0: View?) {
                 numTimesClicked += 1
-                textView?.append("\nThe button got tapped $numTimesClicked time")
+                textView?.append("The button got tapped $numTimesClicked time")
+                if (numTimesClicked != 1) {
+                    textView?.append("$\n")
+                }else{
+                    textView?.append("\n")
+                }
             }
         })
     }
